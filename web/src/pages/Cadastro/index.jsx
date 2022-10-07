22fad6a9cd6dd9782b67666bbf40c8d68f54cf7e
Thiago Back <thiagorback@gmail.com>
2022-10-06 22:56:25 -0300

    Criada a primeira ligação entre front e back end!

--- web/src/pages/Cadastro/index.jsx
+++ web/src/pages/Cadastro/index.jsx
@@ -1,13 +1,48 @@
 import { Icon } from './../../components'
+import axios from 'axios'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import { isValid } from 'date-fns'
 
-const Input = ({name, label, ...props}) => (
+const Input = ({name, label, error, ...props}) => (
     <div className="flex flex-col">
         <label htmlFor={name} className="text-sm font-bold text-red1 mb-2">{label}</label>
-        <input name={name} id={name} {...props} className="p-3 border border-grey1 rounded-xl focus:outline focus:outline-1 focus:outline-red1"/>
+        <input name={name} id={name} {...props} className={`p-3 border border-grey1 rounded-xl focus:outline focus:outline-1 focus:outline-red1 ${error && 'border-red2'}`}/>
+        <span className='p-2 text-sm text-red2' ></span>
     </div>
 )
 
+const validationSchema = yup.object().shape({
+    name: yup.string().required('Preencha seu nome'),
+    username: yup.string().required('Preencha seu nome de usuário'),
+    email: yup.string().email('Informe um e-mail válido!').required('Preencha seu email'),
+    password: yup.string().required('Senha é obrigatória')
+})
+
 export const Cadastro = () => {
+    const formik = useFormik({
+        onSubmit: async (values) => {  //envio das informações para o back end! As validações de campo já foram realizada pelo yup!
+            const res = await axios({
+                method: 'post',
+                baseURL: 'http://192.168.0.10:3000', //endereço temporário
+                url: '/users',
+                data: values
+            })
+
+            console.log(res.data)
+
+        },
+        initialValues: {
+            name: '',
+            username: '',
+            email: '',
+            password: ''
+        },
+        validationSchema
+    })
+
+    console.log(formik.errors)
+    
     return (
         <div>
              <header className="p-4 border-b border-red3">
@@ -26,33 +61,49 @@ export const Cadastro = () => {
                     </h2>
                 </div>
 
-                <form className="space-y-6 p-4">
+                <form className="space-y-6 p-4" onSubmit={formik.handleSubmit}>
                     <Input
                         type="text"
-                        name="nome"
+                        name="name"
                         label="Seu nome"
                         placeholder="Digite seu nome"
+                        error={formik.errors.name && formik.touched.name}
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
                     <Input
                         type="text"
                         name="username"
                         label="Seu nome de usuário"
                         placeholder="Digite um nome de usuário"
+                        error={formik.errors.username && formik.touched.username}
+                        value={formik.values.username}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
                     <Input
-                        type="text"
+                        type="text" 
                         name="email"
                         label="Seu e-mail"
                         placeholder="Digite seu e-mail"
+                        error={formik.errors.email && formik.touched.email}
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
                     <Input
                         type="password"
                         name="password"
                         label="Sua senha"
                         placeholder="Digite uma senha"
+                        error={formik.errors.password && formik.touched.password}
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
                     <div></div>
-                    <button className="text-center w-full  bg-red2 text-white px-8 py-3 rounded-xl">
+                    <button type="submit" className="text-center w-full  bg-red2 text-white px-8 py-3 rounded-xl disabled:opacity-50" disabled={!formik.isValid}>
                         Criar minha conta
                     </button> 
 
